034dda2fe8a39ef464e94c158fa806280782640f
James Brown <jamezbrown100@gmail.com>
2019-06-18 15:07:35 -0500

    add category cell file

---
+++ coder-swag/View/CategoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryCell.swift
+//  coder-swag
+//
+//  Created by James Brown on 6/18/19.
+//  Copyright © 2019 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
